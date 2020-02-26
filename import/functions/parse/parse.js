01cfec55300fce8838096007b9de04fc6a9c5bfb
Fergus McGlynn <theferg@gmail.com>
2020-02-26 17:04:44 +0000

    Add function to find the stressed vowel

--- import/functions/parse/parse.js
+++ import/functions/parse/parse.js
@@ -2,7 +2,15 @@ export const cmuLineParts = (line) => {
     return line.split(" ");
 }
 
-export const wordFromParts = (parts) => {
+export const word = (parts) => {
     return parts[0];
+}
+
+export const stressedVowelIndex = (parts) => {
+    return parts.findIndex(x => /[A-Z]+1/.test(x));
+}
+
+export const stressedVowel = (parts, index) => {
+    return parts[index].slice(0, -1)    
 }
 
--- import/functions/parse/parse.test.js
+++ import/functions/parse/parse.test.js
@@ -1,19 +1,36 @@
-import { wordFromParts, pronunciationFromCMULine, cmuLineParts } from "./parse";
+import { word, cmuLineParts, stressedVowel, stressedVowelIndex } from "./parse";
+
+const parts = ["relax", "R", "IH0", "L", "AE1", "K", "S"];
 
 describe("test cmuLineParts", () => {
 
     it("returns array of line parts", () => {
         const line = "relax R IH0 L AE1 K S";
-        expect(cmuLineParts(line)).toEqual(["relax", "R", "IH0", "L", "AE1", "K", "S"]);
+        expect(cmuLineParts(line)).toEqual(parts);
     });
 
 });
 
-describe("test wordFromCMULine", () => {
+describe("test word", () => {
 
     it("returns the word from CMU line parts", () => {
-        const parts = ["relax", "R", "IH0", "L", "AE1", "K", "S"];
-        expect(wordFromParts(parts)).toEqual("relax");
+        expect(word(parts)).toEqual("relax");
     });
+
+});
+
+describe("test stressVowelIndex", () => {
+
+    it("returns the array index of the stressed vowel", () => {
+        expect(stressedVowelIndex(parts)).toEqual(4); // AE1 is at position 4 when starting from 0
+    });
+
+});
+
+describe("test stressedVowel", () => {
+
+    it("returns the stress from CMU line parts given the stress index", () => {
+        expect(stressedVowel(parts, 4)).toEqual("AE");
+    })
 
 });
