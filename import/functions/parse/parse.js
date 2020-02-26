04f37eed406b4ac8c17910f44eb0611ee10bb9e6
Fergus McGlynn <theferg@gmail.com>
2020-02-26 17:31:30 +0000

    Add function to find the position of stressed vowel

--- import/functions/parse/parse.js
+++ import/functions/parse/parse.js
@@ -14,3 +14,7 @@ export const stressedVowel = (parts, index) => {
     return parts[index].slice(0, -1)    
 }
 
+export const stressedVowelPosition = (parts, index) => {
+    return parts.slice(index + 1).filter(x => /[A-Z]+[02]/.test(x)).length
+}
+
--- import/functions/parse/parse.test.js
+++ import/functions/parse/parse.test.js
@@ -1,12 +1,14 @@
-import { word, cmuLineParts, stressedVowel, stressedVowelIndex } from "./parse";
+import { word, cmuLineParts, stressedVowel, stressedVowelIndex, stressedVowelPosition } from "./parse";
 
-const parts = ["relax", "R", "IH0", "L", "AE1", "K", "S"];
+const parts1 = ["relax", "R", "IH0", "L", "AE1", "K", "S"];
+const parts2 = ["affiliated", "AH0", "F", "IH1", "L", "IY0", "EY2", "T", "IH0", "D"];
+const parts3 = ["detainee", "D", "IY2", "T", "EY0", "N", "IY1"];
 
 describe("test cmuLineParts", () => {
 
     it("returns array of line parts", () => {
         const line = "relax R IH0 L AE1 K S";
-        expect(cmuLineParts(line)).toEqual(parts);
+        expect(cmuLineParts(line)).toEqual(parts1);
     });
 
 });
@@ -14,7 +16,7 @@ describe("test cmuLineParts", () => {
 describe("test word", () => {
 
     it("returns the word from CMU line parts", () => {
-        expect(word(parts)).toEqual("relax");
+        expect(word(parts1)).toEqual("relax");
     });
 
 });
@@ -22,7 +24,7 @@ describe("test word", () => {
 describe("test stressVowelIndex", () => {
 
     it("returns the array index of the stressed vowel", () => {
-        expect(stressedVowelIndex(parts)).toEqual(4); // AE1 is at position 4 when starting from 0
+        expect(stressedVowelIndex(parts1)).toEqual(4); // AE1 is at position 4 when starting from 0
     });
 
 });
@@ -30,7 +32,23 @@ describe("test stressVowelIndex", () => {
 describe("test stressedVowel", () => {
 
     it("returns the stress from CMU line parts given the stress index", () => {
-        expect(stressedVowel(parts, 4)).toEqual("AE");
+        expect(stressedVowel(parts1, 4)).toEqual("AE");
     })
 
+});
+
+describe("test stressedVowelPosition", () => {
+
+    it("returns the position of the stressed syllable when it's the last one", () => {
+        expect(stressedVowelPosition(parts1, 4)).toEqual(0);
+    });
+
+    it("returns the position of the stressed syllable when it's not at the end", () => {
+        expect(stressedVowelPosition(parts2, 3)).toEqual(3);
+    });
+
+    it("returns the correct position when stressed vowel is last sound", () => {
+        expect(stressedVowelPosition(parts3, 6)).toEqual(0);
+    });
+    
 });
